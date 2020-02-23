9a2689ba846bb8e1582a754344d7946650c71106
Arpit Bharti <arpitbharti73@gmail.com>
2020-02-23 13:09:42 +0530

    Improved getURL in ./lib method

--- lib/getURL.ts
+++ lib/getURL.ts
@@ -1,3 +1,18 @@
-const getURL = (): string => process.env.API_URL
+/**
+ * Routes
+ * /expenses?limit=25&offset=25
+ * /expenses/:id
+ * /expenses/:id/receipts
+ */
+const getURL = (requestType: string) => {
+  const baseURL = process.env.API_URL
+  switch (requestType) {
+    case 'GET':
+      return (limit = 10, offset = 0) => `${baseURL}/expenses?limit=${limit}&offset=${offset}`
+    case 'POST':
+      return (id: string, receipts?: boolean) =>
+        receipts ? `${baseURL}/expenses/${id}/receipts` : `${baseURL}/expenses/${id}`
+  }
+}
 
 export default getURL
