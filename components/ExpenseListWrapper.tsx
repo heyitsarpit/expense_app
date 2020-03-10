bb0dba9d8d11123599b132258ec62e81b0e2c4b8
Arpit Bharti <arpitbharti73@gmail.com>
2020-03-11 01:13:44 +0530

    new next

--- components/ExpenseListWrapper.tsx
+++ components/ExpenseListWrapper.tsx
@@ -1,13 +1,19 @@
+import { useContext } from 'react'
+
 import useSelector from '../lib/useSelector'
+import { StaticContext } from '../pages'
 import ExpenseList from './ExpenseList'
 import Pagination from './Pagination'
 
+const isEmpty = <T,>(arr: readonly T[]) => Array.isArray(arr) && arr.length
+
 const ExpenseListWrapper: React.FC = () => {
   const { expenses } = useSelector((state) => state.expenses)
+  const staticExpenses = useContext(StaticContext)
 
   return (
     <div>
-      <ExpenseList expenses={expenses} />
+      <ExpenseList expenses={isEmpty(expenses) ? expenses : staticExpenses} />
       <Pagination />
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,39 @@
+import axios from 'axios'
+import { GetStaticProps } from 'next'
+import { createContext } from 'react'
+
 import AppWrapper from '../components/AppWrapper'
 import Page from '../components/Page'
+import { Expense } from '../components/types'
+import { getURL } from '../lib/resolveURL'
 
-const Home: React.FC = () => {
+interface HomeProps {
+  expenses: Expense[]
+}
+
+export const StaticContext = createContext<Expense[]>([])
+
+const Home: React.FC<HomeProps> = ({ expenses }) => {
   return (
     <Page>
-      <AppWrapper />
+      <StaticContext.Provider value={expenses}>
+        <AppWrapper />
+      </StaticContext.Provider>
     </Page>
   )
 }
 
 export default Home
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const URL = getURL(20, 0)
+
+  const response = await axios.get(URL).catch((error) => console.log(error.message))
+  const { expenses } = response.data as any
+
+  return {
+    props: {
+      expenses
+    }
+  }
+}
